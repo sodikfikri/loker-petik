15dde9e072ecaab6a889c31443f5aab18c3706ed
fajar dwi saputra <fajardwi00737@gmail.com>
2022-06-22 13:32:47 +0700

    implement new loker and tentang UI

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+@section('body_content')
+
+
+<div class="content-wrapper">
+<div class="card" style="border-radius: 2em;">
+    <div class="d-flex justify-content-center mt-5 mb-5">
+        <h4>Tentang Kami</h4>
+    </div>
+    <div class="row">
+        <div class="col-12 mb-4">
+            <div class="row d-flex justify-content-center">
+                    <div class="col-md-4">
+                        <div class="pull-left"><img class="card-img-top" src="assets/images/ikal_petik.png" style="height:278px;width:278px;"/></div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="pull-left" style="height:278px;width:278px;"> <div class="m-4"> <img class="card-img-top" src="assets/images/petik_logo.png" /></div></div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-center m-5">
+        <p class="text-center">Pesantren Teknologi Informasi dan Komunikasi bermula dari keberadaan Lembaga Amil Zakat Infaq dan Shodaqoh PT PLN (Persero) Kantor Pusat (LAZIS PT PLN (Persero) Kantor Pusat) yang dibentuk berdasarkan Keputusan Direksi Nomor 132.K/DIR/2006 Tanggal 11 September 2006 untuk mengelola dana zakat, infaq/ shodaqoh pegawai PLN Pusat.</p>
+    </div>
+</div>
+</div>
+
+@endsection
